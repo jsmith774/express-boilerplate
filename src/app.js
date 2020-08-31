dccb9f8bf1c4dcd6a7718f4758461b3577e49641
Jim <jsmith774@gmail.com>
2020-08-31 09:12:56 -0400

    added db boilerblate

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config.js');
 
+//todo for each entity 'xyz':
+//todo     const xyzRouter = require('./xyx/xyz-router');
+const sampleRouter = require('./sample/sample-router');
+
 const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
@@ -13,9 +17,16 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+//todo refactor routing out of app to use xyzRouter
+//todo      remove direct routing from app
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+//todo for each entity:
+//todo  use imported xyzRouter with common route path base
+//todo     app.use('/v1/xyz');
+app.use('/v1/sample', sampleRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
